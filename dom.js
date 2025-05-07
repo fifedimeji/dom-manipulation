c2b17cbe57457fb6cf0567278d9bab928f1b0424
Fife Oladimeji <fifedimeji@gmail.com>
2025-05-07 12:10:26 +0100

    Initial commit

---
+++ dom.js
@@ -0,0 +1,18 @@
+// EXAMININE THE DOCUMENT //
+
+// console.dir(document)
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+// document.title = 123
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.querySelectorAll("*"))
+console.log(document.querySelectorAll("*")[10])
+// document.all[10].textContent = 'Hello'
+console.log(document.forms)
+console.log(document.links)
+
+
+
